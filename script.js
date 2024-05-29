c85584f799907e8fafe22b89b56901a2b0619457
sayed mudassir <sayedmuddassir68@gmail.com>
2024-05-30 01:29:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const boldText = document.getElementsByTagName("strong");
 function highlight() {
     //Write your code here
-
-
+	 for(let i = 0; i < boldText.length;i++){
+        boldText[i].style.color = "green";
+    }
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+     for(let i = 0; i < boldText.length;i++){
+        boldText[i].style.color = "black";
+    }    
 }
